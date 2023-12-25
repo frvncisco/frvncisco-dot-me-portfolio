bdde150a19a44e0e834fa5f2b35ae9e124dc6a75
Francisco Santana <francisco.santana@niche.com>
2023-12-25 15:36:07 -0500

    update homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">hi, I'm frvncisco👋</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        This is my new portfolio. Currently under constuction🚧
       </p>
     </section>
   );
